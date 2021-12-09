cef3fcf271839a3332fa0c0c0891d2db34c6d559
MarcoB <mrc.bsllt@gmail.com>
2021-12-09 19:50:31 +0100

    Middleware isAuth
    fixed isAuth on client side

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,10 +1,15 @@
 import Cookies from 'js-cookie';
 
-export default function({ req, redirect }) {
+export default function({ req, redirect, store }) {
     let isAuth = true;
 
     if(req) {
         if(!req.headers.cookie) isAuth = false;
+    } else {
+        if(!Cookies.get('token')) {
+            isAuth = false;
+            store.commit('auth/setToken', null);
+        }
     }
 
     isAuth ? console.log('Accesso consentito!') : redirect('/auth/login'); 
